9345d013cc277762219c60091d32406b604f28a2
Joao Gomes <jmarcosfmg@gmail.com>
2023-03-18 23:45:31 -0300

    desafio de use cases

--- src/domain/product/entity/product.interface.ts
+++ src/domain/product/entity/product.interface.ts
@@ -1,4 +1,6 @@
 export default interface ProductInterface {
+  changePrice(price: number): void;
+  changeName(name: string): void;
   get id(): string;
   get name(): string;
   get price(): number;
